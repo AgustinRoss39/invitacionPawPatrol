570b3b2c19de116af0d2b2343bc25b1b0eca8d9d
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 16:40:10 -0300

    Centraliza configuración de invitación Paw Patrol

---
+++ js/config.js
@@ -0,0 +1,47 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Matute",
+    age: 4,
+    message: "Te invito a celebrar mi cumpleaños, pasaremos un rato muy divertido. ¡No faltes!",
+    eventDateTime: "2027-08-27T18:00:00-03:00",
+    eventEndDateTime: "2027-08-27T21:00:00-03:00",
+    dateLabel: "Viernes 27 de Agosto",
+    timeLabel: "18:00 a 21:00 hs.",
+    venue: "Salón de Fiestas Cirilo",
+    address: "Av. Álvarez Thomas 1320",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=Sal%C3%B3n+de+Fiestas+Cirilo+Av.+%C3%81lvarez+Thomas+1320+Buenos+Aires",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Matute. 🐾🚨",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/sounds/audio.mp3",
+    portraitUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/img/foto1.jpg",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/img/1.png",
+      width: "min(104vw, 470px)",
+      top: "-24px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/img/4.png",
+      width: "min(98vw, 440px)",
+      top: "-24px",
+      translateX: "-49%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/img/3.png",
+      width: "min(54vw, 245px)",
+      top: "-4px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionPawPatrol/main/www.tarjetas.cool/modelos/cumple/paw_patrol/img/2.png",
+      width: "min(108vw, 485px)",
+      top: "-62px",
+      translateX: "-52%"
+    }
+  ]
+};
